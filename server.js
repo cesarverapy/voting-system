b812c034609adc79fe1322eaff7c192a6d7e1db0
cesarverapy <cesar.vera.dev@gmail.com>
2024-10-30 18:05:36 -0300

    setup initial express server with json and url-encoded middleware

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); 
+app.use(express.urlencoded({
+    extended:true
+})); 
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+});
